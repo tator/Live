4835f3eb177772ef49812226458ef58ea7844974
Baron Laptop <Baron Laptop@192.168.1.130>
2014-04-23 21:45:30 -0500

    added color to hold

--- src/hold.java
+++ src/hold.java
@@ -6,17 +6,25 @@ import java.awt.Color;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Baron Laptop
  */
 public class hold {
+
     protected int x;
     protected int y;
-    
-    public hold(int x, int y){
+    protected Color c;
+
+    public hold(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
+    public hold(int x, int y, Color c) {
+        this.x = x;
+        this.y = y;
+        this.c = c;
+    }
+
 }
